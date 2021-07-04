a6f687c042c9e122952aedbb42e4521ea5c2e04b
SWATI <SWATI@DESKTOP-RAFFHJ2>
2021-07-04 22:08:14 +0530

    -interviewbit day 35 and MS interview question for immutable class having mutable objects

---
+++ src/com/hackerrank/morganstanley/InputWrapper.java
@@ -0,0 +1,78 @@
+package com.hackerrank.morganstanley;
+
+import java.util.Date;
+
+public class InputWrapper {
+
+	public InputWrapper(Date date, String city, int min, int max) {
+		super();
+		this.date = new Date(date.getTime());
+		this.city = city;
+		this.min = min;
+		this.max = max;
+	}
+
+	private final Date date;
+	private final String city;
+	private final int min;
+	private final int max;
+
+	@Override
+	public String toString() {
+		return "InputWrapper [date=" + date + ", city=" + city + ", min=" + min + ", max=" + max + "]";
+	}
+
+	public final Date getDate() {
+		return new Date(date.getTime());
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public int getMin() {
+		return min;
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + max;
+		result = prime * result + min;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InputWrapper other = (InputWrapper) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (max != other.max)
+			return false;
+		if (min != other.min)
+			return false;
+		return true;
+	}
+
+}
--- src/com/interviewbit/tree/PostOrderNonRecursive.java
+++ src/com/interviewbit/tree/PostOrderNonRecursive.java
@@ -9,27 +9,54 @@ public class PostOrderNonRecursive {
 		root.right = new TreeNode(2);
 		root.right.left = new TreeNode(3);
 		// System.out.println(root);
-		ArrayList<Integer> inorderTraversal = postOrderTraversal(root);
-		System.out.println(inorderTraversal);
+		ArrayList<Integer> postOrderTraversal = postorderTraversalNonRecursive(root);
+		System.out.println(postOrderTraversal);
+		// postorderNonrec(root);
 	}
 
-	public static ArrayList<Integer> postOrderTraversal(TreeNode A) {
-		ArrayList<Integer> result = new ArrayList<>();
-		Stack<TreeNode> s = new Stack<>();
-		TreeNode trav = A;
-		while (trav != null || !s.isEmpty()) {
-			while (trav != null) {
-				s.push(trav);
-				trav = trav.left;
-			}
-			if (!s.isEmpty()) {
-				trav = s.pop();
-				if (trav.right != null) {
-					trav = trav.right;
-					result.add(trav.val);
-				}
-			}
+	public static ArrayList<Integer> postorderTraversalNonRecursive(TreeNode root) {
+		ArrayList<Integer> list = new ArrayList<>();
+		Stack<TreeNode> s1 = new Stack<>();
+		Stack<TreeNode> s2 = new Stack<>();
+		s1.push(root);
+		while (!s1.isEmpty()) {
+			TreeNode poppedS1 = s1.pop();
+			s2.push(poppedS1);
+			
+			if(poppedS1.left != null)
+				s1.push(poppedS1.left);
+			if(poppedS1.right != null)
+				s1.push(poppedS1.right);
 		}
-		return result;
+		while (!s2.isEmpty()) {
+			list.add(s2.pop().val);
+		}
+		return list;
 	}
+
+	public static ArrayList<Integer> postorderTraversal(TreeNode trav) {
+		ArrayList<Integer> list = new ArrayList<>();
+		if (trav == null)
+			return list;
+		postorderTraversal(trav, list);
+		return list;
+	}
+
+	public static void postorderTraversal(TreeNode trav, ArrayList<Integer> list) {
+		if (trav == null)
+			return;
+		postorderTraversal(trav.left, list);
+		postorderTraversal(trav.right, list);
+		list.add(trav.val);
+	}
+
+	/*
+	 * public static void postorderNonrec(TreeNode A) { System.out.print("POST: ");
+	 * Stack<TreeNode> s = new Stack<>(); TreeNode trav = A; while (trav != null ||
+	 * !s.isEmpty()) { while (trav != null) { s.push(trav); trav = trav.left; } if
+	 * (!s.isEmpty()) { trav = s.pop(); if (trav.right == null ||
+	 * trav.right.visited) { System.out.print(trav.val + ", "); trav.visited = true;
+	 * trav = null; } else { s.push(trav); trav = trav.right; } } }
+	 * System.out.println(); }
+	 */
 }
