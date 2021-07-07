e667425aef57457fbcbbbb8b2b1c3ab2e85bab54
SWATI <SWATI@DESKTOP-RAFFHJ2>
2021-07-07 12:36:43 +0530

    -improved solution

--- src/com/interviewbit/linkedlist/SortBinaryLinkedList.java
+++ src/com/interviewbit/linkedlist/SortBinaryLinkedList.java
@@ -5,48 +5,44 @@ import java.util.Arrays;
 public class SortBinaryLinkedList {
 	public static void main(String[] args) {
 
-		//ListNode a = LinkedListUtil.createNodesFromList(Arrays.asList(1, 0, 0, 1));
+		ListNode a = LinkedListUtil.createNodesFromList(Arrays.asList(1, 0, 0, 1));
 		//ListNode a = LinkedListUtil.createNodesFromList(Arrays.asList(0,0,1,1,0));
-		ListNode a = LinkedListUtil.createNodesFromList(Arrays.asList(1,0));
+		//ListNode a = LinkedListUtil.createNodesFromList(Arrays.asList(1,0));
 		ListNode sortedNode = solve(a);
 		LinkedListUtil.printList(sortedNode);
 	}
 
 	public static ListNode solve(ListNode A) {
-		int[] arr = new int[2];
-		ListNode result=A;
+		int zeroCnt = 0;
+		int oneCnt = 0;
+		ListNode copyOfA=A;
 		while (A != null) {
 			if (A.val == 0)
-				arr[0] += 1;
+				zeroCnt += 1;
 			else
-				arr[1] += 1;
+				oneCnt += 1;
 			A = A.next;
 		}
-		LinkedListUtil.printList(result);
-		LinkedListUtil.printList(A);
-		int zeroCnt = arr[0];
-		int oneCnt = arr[1];
-		ListNode headNode = null;
+		//LinkedListUtil.printList(copyOfA);
+		//LinkedListUtil.printList(A);
+		ListNode headNode = copyOfA;
 		if (zeroCnt > 0) {
-			headNode = new ListNode(0);
+			headNode.val=0;
 			ListNode parent = headNode;
 			for (int i = 1; i < zeroCnt; i++) {
-				ListNode newNode = new ListNode(0);
-				parent.next = newNode;
-				parent = newNode;
+				parent.next.val = 0;
+				parent = parent.next;
 			}
 			for (int i = 0; i < oneCnt; i++) {
-				ListNode newNode = new ListNode(1);
-				parent.next = newNode;
-				parent = newNode;
+				parent.next.val=1;
+				parent = parent.next;
 			}
 		} else if (oneCnt > 0) {
-			headNode = new ListNode(1);
+			headNode.val=1;
 			ListNode parent = headNode;
 			for (int i = 1; i < oneCnt; i++) {
-				ListNode newNode = new ListNode(1);
-				parent.next = newNode;
-				parent = newNode;
+				parent.next.val=1;
+				parent = parent.next;
 			}
 		}
 		return headNode;
